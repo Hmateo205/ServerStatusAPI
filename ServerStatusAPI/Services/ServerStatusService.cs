b9b0053cc96bdde09b2fa6984ac4700f5ded7096
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2024-07-18 20:15:51 -0500

    servicio

---
+++ ServerStatusAPI/Services/ServerStatusService.cs
@@ -0,0 +1,28 @@
+﻿using ServerStatusAPI.Models;
+using System.Diagnostics;
+
+
+namespace ServerStatusAPI.Services
+{
+    public class ServerStatusService
+    {
+        public ServerStatus GetServerStatus()
+        {
+            var status = new ServerStatus();
+            var currentProcess = Process.GetCurrentProcess();
+            var memoryUsage = currentProcess.WorkingSet64;
+
+            if (memoryUsage == 0)
+            {
+                status.Status = "Apagado";
+            }
+            else
+            {
+                status.Status = "En Ejecución";
+                status.MemoryUsage = memoryUsage;
+            }
+
+            return status;
+        }
+    }
+}
