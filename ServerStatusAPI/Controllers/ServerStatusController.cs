6b5a7c1cad26eb0f9c4a5fdd70a57686f7bd0e09
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2024-07-18 20:18:45 -0500

    Controler

---
+++ ServerStatusAPI/Controllers/ServerStatusController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServerStatusAPI.Models;
+using ServerStatusAPI.Services;
+
+namespace ServerStatusAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServerStatusController : ControllerBase
+    {
+        private readonly ServerStatusService _serverStatusService;
+
+        public ServerStatusController(ServerStatusService serverStatusService)
+        {
+            _serverStatusService = serverStatusService;
+        }
+
+        [HttpGet]
+        public ActionResult<ServerStatus> Get()
+        {
+            var status = _serverStatusService.GetServerStatus();
+            return Ok(status);
+        }
+    }
+}
+
+}
